a7e67df6cfd7c11f53c7bd685d08188ce783b434
hussein_belaifa <belaifahocine@gmail.com>
2022-12-12 11:23:19 -0500

    create a pipeline file

---
+++ pipeline.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage("checkout"){
+            steps {
+
+                git url: 'git@github.com:husseinbelaifa/spring-petclinic-jenkins.git', branch: 'main'
+
+
+            }
+
+        }
+
+
+        stage('Build') {
+            steps {
+
+
+                sh "./mvnw clean package"
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
